86a498d4cb595c26dcb7f9038dfb9612410ba497
Gustavo Franco <gustavofrancodias@hotmail.com>
2022-07-25 00:36:58 -0300

    Dungeon rooms generation

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 
 public class Room : MonoBehaviour
 {
@@ -9,4 +9,25 @@ public class Room : MonoBehaviour
     public int height;
     public int x;
     public int y;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
+    }
+
+    void Start()
+    {
+        if(RoomController.instance == null)
+        {
+            throw new Exception("Non playable scene has been started");
+        }
+
+        RoomController.instance.RegisterRoom(this);
+    }
+
+    public Vector2 GetRoomCenter()
+    {
+        return new Vector2(x *width, y * height);
+    }
 }
--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomInfo
 {
@@ -12,7 +13,7 @@ public class RoomInfo
 public class RoomController : MonoBehaviour
 {
     public static RoomController instance;
-    string currentWorldName = "Basement";
+    string currentWorldName = "Dungeon";
     RoomInfo currentLoadRoomData;
     Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
     public List<Room> loadedRooms = new List<Room>();
@@ -23,8 +24,80 @@ public class RoomController : MonoBehaviour
         instance = this;
     }
 
+    void Start()
+    {
+        LoadRoom("Start", 0, 0);
+        LoadRoom("Room", 1, 0);
+        LoadRoom("Room", -1, 0);
+        LoadRoom("Room", 0, 1);
+        LoadRoom("Room", 0, -1);
+    }
+
+    void Update()
+    {
+        UpdateRoomQueue();
+    }
+
+    void UpdateRoomQueue()
+    {
+        if (isLoadingRoom)
+        {
+            return;
+        }
+
+        if (loadRoomQueue.Count == 0)
+        {
+            return;
+        }
+        currentLoadRoomData = loadRoomQueue.Dequeue();
+        isLoadingRoom = true;
+
+        StartCoroutine(LoadRommRoutine(currentLoadRoomData));
+    }
+
     public bool DoesRoomExists(int x, int y)
     {
         return loadedRooms.Find(item => item.x == x && item.y == y) != null;
+    }
+
+    public void LoadRoom(string name, int x, int y)
+    {
+        if (DoesRoomExists(x, y))
+        {
+            return;
+        }
+        RoomInfo newRoomData = new RoomInfo();
+        newRoomData.name = name;
+        newRoomData.x = x;
+        newRoomData.y = y;
+
+        loadRoomQueue.Enqueue(newRoomData);
+    }
+
+    IEnumerator LoadRommRoutine(RoomInfo info)
+    {
+        string roomName = currentWorldName + info.name;
+        AsyncOperation loadRoom = SceneManager.LoadSceneAsync(roomName, LoadSceneMode.Additive);
+        while (loadRoom.isDone == false)
+        {
+            yield return null;
+        }
+    }
+
+    public void RegisterRoom(Room room)
+    {
+        room.transform.position = new Vector2(
+            currentLoadRoomData.x = room.width,
+            currentLoadRoomData.y = room.height
+        );
+
+        room.x = currentLoadRoomData.x;
+        room.y = currentLoadRoomData.y;
+        room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.x + ", " + room.y;
+        room.transform.parent = transform;
+
+        isLoadingRoom = false;
+
+        loadedRooms.Add(room);
     }
 }
